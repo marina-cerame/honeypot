0a0f72ed4687b777fb595f6407ef243410fd41a2
Marina Cerame <marina.cerame@gmail.com>
2017-01-16 17:21:09 -0600

    fix database - add relationships

--- honeypot-api/app/relationships.js
+++ honeypot-api/app/relationships.js
@@ -12,5 +12,19 @@ const Nodal = require('nodal');
   Comment.joinsTo(Post, {multiple: true});
 
 */
+const Item = Nodal.require('app/models/item.js');
+const PetType = Nodal.require('app/models/pet_type.js');
+const Pet = Nodal.require('app/models/pet.js');
+const Transaction = Nodal.reuire('app/models/transaction.js');
+const User = Nodal.require('app/models/user.js');
+
+Pet.joinsTo(User, {multiple: true});
+Pet.joinsTo(PetType, {multiple: true});
+Item.joinsTo(PetType, {multiple: true});
+Transaction.joinsTo(User, {multiple: true});
+Transaction.joinsTo(Pet, {multiple: true});
+Transaction.joinsTo(Item, {multiple: true});
+
+
 
 module.exports = {}; // Don't need to export anything
